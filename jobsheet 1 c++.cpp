9fc5b404a2329dda3c6fe243a41974c87bf8dd8b
Lalu Tohirul Akhyar <64624052+LaluTohirulAkhyar@users.noreply.github.com>
2020-05-12 23:22:49 +0800

    Create jobsheet 1 c++.cpp
    menampilkan array 1 dimensi

---
+++ jobsheet 1 c++.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std; 
+ int main()
+ {
+ 	cout<<"nama :LALU TOHIRUL AKHYAR -F1B019081\n";
+ 	cout<<"NIM : f1b019081\n";
+ 	
+	int nim [11]={1,2,3,4,5,6,7,8,9,10,11};
+	cout<<"NIM 1="<<nim [0]<<endl;
+	cout<<"NIM 2="<<nim[1]<<endl;
+	cout<<"NIM 3="<<nim [2]<<endl;
+	cout<<"NIM 4="<<nim [3]<<endl;
+	cout<<"NIM 5="<<nim [4]<<endl;
+	cout<<"NIM 6="<<nim [5]<<endl;
+	cout<<"NIM 7="<<nim [6]<<endl;
+	cout<<"NIM 8="<<nim [7]<<endl;
+	cout<<"NIM 9="<<nim [8]<<endl;
+	cout<<"NIM 10="<<nim [9]<<endl;
+	cout<<"NIM 11="<<nim [10]<<endl;
+	
+	return 0;
+}
+
